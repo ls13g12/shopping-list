1926ba24c9af552b2fed173201c52bb50e4ed1e2
ls13g12 <luke.stoneman.94@gmail.com>
2022-05-02 21:45:52 +0100

    fixed api bp import

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint("api", __name__)
 
-from app.api import item, recipe, selected_dates_log, recipe_dates
+from app.api import item, recipe, selected_dates_log
